9a5ec252b27588d77d2054df724bbc63e1108206
AdreReyes <108366142+AdreReyes@users.noreply.github.com>
2022-10-15 06:21:58 -0400

    added test

--- test_app.py
+++ test_app.py
@@ -1,10 +1,16 @@
-from application import app, greet
+from application import app, greet import 
+from flask import Flask
+from flask import request
+
 
 def test_quick():
   a = "jeff"
   greeting = greet(a)
   assert greeting == "Hi jeff"
+  
+def test_home_page():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    print("Done.")
 
-# def test_home_page():
-#     response = app.test_client().get('/')
-#     assert response.status_code == 200
+
